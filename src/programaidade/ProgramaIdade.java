b6f09c5abca3a8bacaa3a190cf6fee0f7bc2e0b3
Kaio Sumikawa <kaiosumikawa@gmail.com>
2025-12-13 15:52:56 +0900

    Aula 10 – Estruturas Condicionais (Parte 2)

---
+++ src/programaidade/ProgramaIdade.java
@@ -0,0 +1,17 @@
+package programaidade;
+import java.util.Scanner;
+
+public class ProgramaIdade {
+    public static void main(String[] args) {
+        Scanner t = new Scanner(System.in);
+        System.out.println("Em que ano você nasceu? ");
+        int nasc = t.nextInt();
+        int i = 2015-nasc;
+        System.out.println("Sua idade é " + i);
+        if (i>=18) {
+            System.out.println("Maior de Idade");
+        } else {
+            System.out.println("Menor de Idade");
+        }
+    }
+}
