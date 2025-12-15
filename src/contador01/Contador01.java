eaf9e21595e90784eb1cb1e9201efab4c0b50d1b
Kaio Sumikawa <kaiosumikawa@gmail.com>
2025-12-15 10:34:40 +0900

    Aula 11 – Estruturas de Repetição (Parte 1)

---
+++ src/contador01/Contador01.java
@@ -0,0 +1,14 @@
+package contador01;
+
+public class Contador01 {
+    public static void main(String[] args) {
+        int cc = 0;
+        while (cc<10) {
+            cc++;
+            if (cc == 5 || cc == 7 || cc == 9) {
+                continue;
+            }
+            System.out.println("Cambalhota " + cc);
+        }
+    }
+}
